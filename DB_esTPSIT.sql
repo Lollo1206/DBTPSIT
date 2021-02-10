bb50fe30a4c1b1d24791b9c293821d16125b7af6
Lollo1206 <50620353+Lollo1206@users.noreply.github.com>
2021-02-10 09:24:09 +0100

    Add files via upload

---
+++ DB_esTPSIT.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE esTPSIT;
+#DROP DATABASE esTPSIT;
+USE esTPSIT;
+
+CREATE TABLE Attori(
+	Nome VARCHAR(20),
+	Cognome VARCHAR(20)
+);
+
+INSERT INTO Attori(Nome, Cognome)
+VALUES
+("Bruce", "Lee"),
+("Christian","De Sica"),
+("Bud","Spencer"),
+("Terence","Hill"),
+("Johnny","Depp"),
+("Gigi","Proietti"),
+("Leonardo","Di Caprio"),
+("Chris","Evans"),
+("Francesco","Mandelli"),
+("Jennifer","Aniston");
